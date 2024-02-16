65618040856b4f0d9081163dcd37a80b8a07b114
BT_4N2_20 <DSGF@ADS>
2024-02-16 13:20:24 -0500

    Updated code

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,15 @@ public class Main {
         double tv = 0; //I will not be using a tv
         double fun = 80;
         double monthlyStudentLoan = 845;
-        double savingsAccount = 25.6; //this was the money left after monthly expenses
+        double savingsAccount = 0; //this was the money left after monthly expenses
         int months = 0;
         while (savingsAccount < 150000) {
             months++;
             if (months % 12 == 0) { //meaning yearly inflation
-                yearlyBonus = (salary * (((Math.random() * 6) + 7) /100) * 0.78);
+                if ((int) (Math.random() * 6) + 1 == 1) { //random chance of successful year
+                    yearlyBonus = (salary * (((Math.random() * 6) + 7) /100) * 0.78);
+                    savingsAccount += yearlyBonus; //you get a bonus!!
+                }
                 salary = salary + salary * (((Math.random() * 3) + 5) /100);
                 rent *= 1 + (((Math.random() * 3) + 3) / 100);
                 rentInsurance *= 1 + (((Math.random() * 3) + 3) / 100);
@@ -31,8 +34,6 @@ public class Main {
                 monthlyStudentLoan = 0;
             }
             savingsAccount += (salary * 0.48)/12; //0.48 because 0.5 aka 50% went to taxes, and 0.02 aka 2% went to retirement
-            savingsAccount += yearlyBonus;
-            savingsAccount -= monthlyStudentLoan;
             savingsAccount -= rent;
             savingsAccount -= rentInsurance;
             savingsAccount -= wifi;
@@ -44,7 +45,7 @@ public class Main {
             if ((int) (Math.random() * 5) + 1 == 1) {
                 savingsAccount -= 150;
             }
-            if (savingsAccount > 150000 && monthlyStudentLoan != 0){
+            if (monthlyStudentLoan != 0){
                 savingsAccount = savingsAccount - monthlyStudentLoan;
             }
         }
